056fa224eab8063e7bbc28a84dd617f916fe1403
maruuroman <maria.roman@correo.ucu.edu.uy>
2023-10-22 13:33:34 -0300

    ejercicio 1 terminado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,25 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+             //Creo los filtros
+            IFilter filter1 = new FilterGreyscale(); // Primer filtro: Escala de grises
+            IFilter filter2 = new FilterNegative();   // Segundo filtro: Negativo
+
+            //Uso IPipe
+            IPipe pipe1 = new PipeSerial(filter1, new PipeNull()); // Pipe para el primer filtro
+            IPipe pipe2 = new PipeSerial(filter2, new PipeNull()); // Pipe para el segundo filtro
+
+            //Uno los IPipe con las imagenes 
+            pipe1.Send(picture); // Aplicar el primer filtro
+            IPicture image = pipe2.Send(picture); // Aplicar el segundo filtro
+
+            //Guardo la imagen con los filtros aplicados 
+            provider.SavePicture(image, @"beer.jpg");
+           
+            Console.WriteLine("Proceso completado."); //chequo que este realizando el codigo
         }
     }
 }
